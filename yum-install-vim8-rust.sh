dc121a999ac73aa5695161f2911a6a2c18b9ed65
Bren Moushall <bmoush@gmail.com>
2019-03-28 04:29:36 +0000

    Fixes.

--- yum-install-vim8-rust.sh
+++ yum-install-vim8-rust.sh
@@ -1,3 +1,4 @@
+curdir=`pwd`
 #!/bin/bash
 sudo yum -y install epel-release
 sudo yum -y update
@@ -10,6 +11,8 @@ sudo yum -y install unzip
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 rustup update
+sudo yum -y install openssl-devel
+sudo yum -y install cmake
 
 # install terraform
 sudo curl -L https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip \
@@ -26,19 +29,21 @@ git config --global color.ui auto
 . /etc/bash_completion.d/git
 
 # VIM
-cd /home/centos/.vim/
-mkdir -p pack/plugins/start
-
+sudo yum -y install ncurses-devel
+cd /usr/tmp
+git clone https://github.com/vim/vim
+cd vim
+make
+make install
+cd $curdir
+mkdir -p ~/.vim/pack/plugins/start
 # Plugins
 git clone https://github.com/w0rp/ale.git ~/.vim/pack/plugins/start/ale
 git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
 
 # link to the dot files
-curdir=`pwd`
-cd ~
-ln -fs "$curdir"/vimrc .vimrc
-ln -fs "$curdir"/tmux.conf .tmux.conf
-ln -fs "$curdir"/bash_aliases .bash_aliases # ensure that .bashrc loads this
-ln -fs "$curdir"/bashrc .bashrc
-cd "$curdir"
+ln -fs "$curdir"/vim8/vimrc ~/.vimrc
+ln -fs "$curdir"/tmux.conf ~/.tmux.conf
+ln -fs "$curdir"/bash_aliases ~/.bash_aliases # ensure that .bashrc loads this
+ln -fs "$curdir"/bashrc ~/.bashrc
 
