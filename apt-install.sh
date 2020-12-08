39ab49c4096e7f0dae45f3a7e57fe6d3c1917431
Bren Moushall <bmoush@gmail.com>
2020-12-09 10:46:26 +1100

    git-prompt and completion are different'

--- apt-install.sh
+++ apt-install.sh
@@ -59,4 +59,3 @@ git config --global user.name "Bren Moushall"
 git config --global user.email "bmoush@gmail.com"
 git config --global push.default simple
 git config --global color.ui auto
-. /etc/bash_completion.d/git-prompt
