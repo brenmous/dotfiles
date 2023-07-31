98e1c4e5ca8b89d2b7e9463dc98a3a98dfe117da
Brenainn Moushall <bmoush@gmail.com>
2023-07-31 12:21:47 +1000

    Update nvim config
    LSP updates, autocompletion

--- common/conf/nvim/init.lua
+++ common/conf/nvim/init.lua
@@ -25,31 +25,95 @@ packer.startup(function(use)
         use("themercorp/themer.lua")
 	use("nvim-lualine/lualine.nvim")
 	use("ctrlpvim/ctrlp.vim")
-	use("github/copilot.vim")
         use("neovim/nvim-lspconfig")
+	use("ggandor/leap.nvim")
+        use("hrsh7th/nvim-cmp")
+        use("hrsh7th/cmp-nvim-lsp")
+        use("saadparwaiz1/cmp_luasnip")
+        use("L3MON4D3/LuaSnip")
 end)
 
 --plugin config
 ---- themer - lsp and treesitter compatible colorschemes
+--
 ---- use :SCROLLCOLOR to preview the available themes
 require("themer").setup({
-    colorscheme="nord",
-    styles={
-        comment = {fg = "#707D97"},
-        string = {fg = "#89A571"},
-        ["function"] = {fg = "#88C0D0"}
-    }
+    colorscheme="jellybeans"
+    ---styles={
+    ---    comment = {fg = "#707D97"},
+    ---    string = {fg = "#89A571"},
+    ---    ["function"] = {fg = "#88C0D0"}
+    ---}
 })
 
 ---- lsp setup
 ---- available servers and configurations:
 ---- https://github.com/neovim/nvim-lspconfig/blob/044388b0d437c2f266d69231421e99b1598cc39c/doc/server_configurations.md
-require('lspconfig').pyright.setup({})
-require('lspconfig').cssls.setup({})
-require('lspconfig').eslint.setup({})
-require('lspconfig').html.setup({})
-require('lspconfig').terraformls.setup({})
-require('lspconfig').jsonls.setup({})
+--require('lspconfig').pyright.setup({})
+local lspconfig = require('lspconfig')
+pylsp_settings = {
+    pylsp = {
+        plugins = {
+            pycodestyle = {
+                maxLineLength = 100
+            }
+        }
+    }
+}
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local servers = { pylsp=pylsp_settings, cssls={}, eslint={}, html={}, terraformls={}, jsonls={} }
+for lsp, settings in pairs(servers) do
+    lspconfig[lsp].setup {
+        settings = settings,
+        capabilities = capabilities
+    }
+end
+
+---- luasnip
+local luasnip = require('luasnip')
+
+---- nvim-cmp
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 ---- ctrlp
 vim.g.ctrlp_custom_ignore = 'git'
@@ -66,6 +130,9 @@ require('nvim-treesitter.configs').setup({
     }
 })
 
+---- leap
+require('leap').set_default_keymaps()
+
 --mappings
 ---- leader
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
@@ -79,22 +146,53 @@ vim.keymap.set("n", "<leader>pr", ":CtrlPMRU<cr>")
 ---- unhighlight
 vim.keymap.set("n", "<leader>h", ":noh<cr>")
 
----- copilot accept
-vim.keymap.set("i", "<S-tab>", "copilot#Accept('<CR>')", { silent = true, expr = true })
-
 ---- timestamp insert
 vim.keymap.set("n", "<F5>", "=strftime('%Y-%m-%d %H:%M:%S')<CR>P", { noremap = true })
 vim.keymap.set("i", "<F5>", "<C-R>=strftime('%Y-%m-%d %H:%M:%S')<CR>", { noremap = true })
 
 ---- lsp
-vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.goto_next()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>w", "<cmd>lua vim.diagnostic.goto_prev()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<leader>qq", ":cclose<cr>", {noremap=true, silent=true})
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<leader>e', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>E', vim.diagnostic.goto_next)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+
+
+---- lsp
+--vim.keymap.set("n", "<leader>E", "<cmd>lua vim.diagnostic.goto_next()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>e", "<cmd>lua vim.diagnostic.goto_prev()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.declaration()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap=true, silent=true})
+--vim.keymap.set("n", "<leader>qq", ":cclose<cr>", {noremap=true, silent=true})
 
 
 --options
