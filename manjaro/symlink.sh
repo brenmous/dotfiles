7d1a0fd3bbc97a5d4481cfa69cc80ed36cd6a80f
Brenainn Moushall <bmoush@gmail.com>
2023-11-03 09:26:13 +1000

    Update bits and pieces

--- manjaro/symlink.sh
+++ manjaro/symlink.sh
@@ -1,8 +1,11 @@
 curdir=$(pwd)
 cd ~
+mkdir -p .config
+mkdir -p .docker
 ln -sfn "$curdir"/../common/conf/tmux.conf .tmux.conf
 ln -sfn "$curdir"/conf/bash_aliases .bash_aliases
 ln -sfn "$curdir"/conf/bashrc .bashrc
 ln -sfn "$curdir"/../common/conf/nvim .config/
+ln -sfn "$curdir"/conf/docker/config.json .docker/
 cd "$curdir"
 
