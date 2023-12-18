6efd5ada76b8f11d507050f0d9532128f1bad3ed
Bren Moushall <bmoush@gmail.com>
2023-12-18 19:26:01 +1100

    Updates for new ubuntu 22.04 vm

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+# starting from a minimal install of Ubuntu 22.04 desktop
+
 set -e 
+
+curdir=$(pwd)
 
 echo "Updating ubuntu"
 sudo apt update && sudo apt upgrade
+
+echo "Installing required packages"
+sudo apt install git \
+    curl \
+    gcc \
+    software-properties-common \
+    xclip \
+    nodejs \
+    npm \
+    gnupg \
+    ca-certificates \
+    python3-pip
 
 echo "Installing nvim nightly"
 ../common/install-nvim.sh
 
 echo "Updating pip and setuptools"
-python -m pip install -U pip setuptools
+python3 -m pip install -U pip setuptools
 
 echo "Installing quickenv"
 pip install git+https://github.com/brenmous/quickenv
@@ -17,8 +33,38 @@ pip install git+https://github.com/brenmous/quickenv
 echo "Installing pyright"
 pip install pyright
 
+echo "Installing vscode language servers"
+sudo npm i -g vscode-langservers-extracted
+
 echo "Installing tmux"
 sudo apt install tmux
+
+echo "Adding hashicorp repos"
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update 
+
+echo "Installing terraform"
+sudo apt install terraform
+
+echo "Installing packer"
+sudo apt install packer
+
+echo "Installing docker"
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Installing amazon-ecr-credential-helper"
+sudo apt install amazon-ecr-credential-helper
 
 echo "Linking config files"
 ./symlink.sh
