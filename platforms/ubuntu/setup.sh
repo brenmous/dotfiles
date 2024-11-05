a344630330bada083c00b080ad5710e5c5eb8d81
Brenainn Moushall <>
2024-11-05 14:21:05 +1100

    Update setup for 24.10

--- platforms/ubuntu/setup.sh
+++ platforms/ubuntu/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# starting from a minimal install of Ubuntu 22.04 desktop
+# Ubuntu 24.10
 
 set -e
 
@@ -19,20 +19,19 @@ sudo apt install git \
     ca-certificates \
     python3-pip \
     pipx \
-    tmux
-
-echo "Installing nvim nightly"
-../common/install-nvim.sh
-
-echo "Updating pip and setuptools"
-python3 -m pip install -U pip
+    tmux \
+    libfuse2t64 \
+    golang \
+    python3-dev
 
 echo "Installing uv"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo "Adding hashicorp repos"
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+# No hashicorp repos for 24.10 yet, use 24.04 (doesn't matter anyway, no difference)
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com noble main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 
 echo "Installing terraform, packer"
@@ -48,15 +47,10 @@ echo \
     sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
 sudo usermod -aG docker "$USER"
-newgrp docker
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 sudo apt install amazon-ecr-credential-helper
-
-echo "Linking config files"
-../../symlink.sh
 
 echo "Configuring git"
 git config --global user.name "Brenainn Moushall"
