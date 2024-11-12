683696afc911eeaeab8e402f709b945036ec58e3
Bren Moushall <bmoush@gmail.com>
2024-11-12 00:09:37 +0000

    Fail on error

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Update to latest nvim image
+set -e
+
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o /tmp/nvim.appimage
 sudo mv /tmp/nvim.appimage /usr/local/bin/nvim
 chmod u+x /usr/local/bin/nvim
