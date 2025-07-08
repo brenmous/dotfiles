e106201dac2b086ee9cb5465c2008480d78ce91a
Brenainn Moushall <bmoush@gmail.com>
2025-07-08 18:17:05 +1000

    Update nightly nvim url

--- nvim/install-nvim.sh
+++ nvim/install-nvim.sh
@@ -2,6 +2,6 @@
 # Update to latest nvim image
 set -e
 
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o /tmp/nvim.appimage
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage -o /tmp/nvim.appimage
 sudo mv /tmp/nvim.appimage /usr/local/bin/nvim
 chmod u+x /usr/local/bin/nvim
